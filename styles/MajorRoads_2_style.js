089e8b3b0f95e814ccc35e911138e42958947f82
Adedokun Toluwanimi <adedokuntoluwanimi@mtu.edu.ng>
2025-12-09 13:54:32 +0000

    Delete styles directory

--- styles/MajorRoads_2_style.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4a902d1a719049a07a7f0b9458cc890626b044f0a8e689bed3f4b9f3ca0ccc2b
-size 1109
--- styles/WardBoundaries_0_style.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:103209b49f9d41e4de67d7f15cbd29d862d33c1a7d80162857477dc48375b7d8
-size 1160
